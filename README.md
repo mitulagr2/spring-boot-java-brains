faf96d790848e297f94c79de2e1f854f5cba2ff4
mitulagrofficial <mitulagr4@gmail.com>
2022-10-12 17:52:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-boot-java-brains
+ 
---
+++ course-api-data/src/main/java/com/mitul/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.mitul.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
